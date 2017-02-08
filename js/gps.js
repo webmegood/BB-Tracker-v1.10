6e46b15483dd6e3061095f62db93730517c402e0
webmegood <webmegood@gmail.com>
2017-02-08 17:21:22 +1100

    Add files via upload

--- js/gps.js
+++ js/gps.js
@@ -92,9 +92,9 @@ function setupGeolocation() {
 	
 	
 
-    var failureFn = function(error) {
-        console.log('BackgroundGeolocation error');
-    };
+    //var failureFn = function(error) {
+        //console.log('BackgroundGeolocation error');
+    //};
 	
 	
 	
@@ -106,7 +106,7 @@ function setupGeolocation() {
         desiredAccuracy: 10,
         stationaryRadius: 10,
         distanceFilter: 10,
-    	//interval: 30000, // <!-- poll for position every 30 secs 
+    	interval: 10000, // <!-- poll for position every 10 secs 
 		//locationService: backgroundGeoLocation.service.ANDROID_FUSED_LOCATION,
         debug: false, // <-- enable this hear sounds for background-geolocation life-cycle.
         stopOnTerminate: true // <-- enable this to clear background location settings when the app terminates
