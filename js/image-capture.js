0e5cb6f52567cae47155aa2e236cb45c357df0a4
webmegood <webmegood@gmail.com>
2017-02-09 00:22:25 +1100

    Add files via upload

--- js/image-capture.js
+++ js/image-capture.js
@@ -1,26 +1,9 @@
 $(document).ready(".takePicBtn").click(function() {
 
 // Take picture using device camera and retrieve image as base64-encoded string
+alert("yes!");
     navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50 });
 });
-
-
-//Callback function when the picture has been successfully taken
-function onPhotoDataSuccess(imageData) {                
-    // Get image handle
-    var smallImage = document.getElementById('smallImage');
-
-    // Unhide image elements
-    smallImage.style.display = 'block';
-    smallImage.src = imageData;
-}
-
-//Callback function when the picture has not been successfully taken
-function onFail(message) {
-    alert('Failed to load picture because: ' + message);
-}
-
-
 
 
 
