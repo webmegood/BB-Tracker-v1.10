4b871135f274701c686ebe07a33720c840c955ec
webmegood <webmegood@gmail.com>
2017-03-29 10:53:49 +1100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -397,32 +397,18 @@ $("#btn-track-control").click(function(){
 
 $(document).ready(function() {
 	var viewPortHeight = $(window).height();
-alert (viewPortHeight);
-//$('html').css("height", 534);
+	alert (viewPortHeight);
 });
 
 
- $('.menul').addClass('red').delay(1000).queue(function(next){
-         $(this).removeClass('red');
-         next();
-    });
 
 
 //force initial screen to adjust (necessary because viewport otherwise won't be full height)
 $(document).ready(function() {
-	var deviceWidth = $(window).width();
-	var deviceHeight = $(window).height();
-	if ( deviceWidth < deviceHeight ) {
 		$('html').addClass('forceHeight').delay(1000).queue(function(next){
 			$(this).removeClass('forceHeight');
 			$(this).addClass('normaliseHeight');
 			next();
 		});
-	} else {
-		$('html').addClass('forceHeight').delay(1000).queue(function(next){
-			$(this).removeClass('forceHeight');
-			$(this).addClass('normaliseHeight');
-		});
-	}
 });
 
